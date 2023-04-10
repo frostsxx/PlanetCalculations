12ae9d07c21d75c7ef2f05859453883e1ad92653
frostsxx <henrique.geadas@gmail.com>
2023-04-10 16:55:36 +0100

    format changes

--- PlanetCalculations/Program.cs
+++ PlanetCalculations/Program.cs
@@ -7,73 +7,72 @@ namespace PlanetCalculations
         static void Main(string[] args)
         {
 
-                // Choose a planet
-                Console.WriteLine("Choose a planet number (1 - Mercury / 2 - Venus / 3 - Earth / 4 - Mars / 5 - Jupiter / 6 - Saturn / 7 - Uranus / 8 - Neptune): ");
-                var planet = int.Parse(Console.ReadLine());
+            // Choose a planet
+            Console.WriteLine("Choose a planet number (1 - Mercury / 2 - Venus / 3 - Earth / 4 - Mars / 5 - Jupiter / 6 - Saturn / 7 - Uranus / 8 - Neptune): ");
+            var planet = int.Parse(Console.ReadLine());
+
+            while (planet < 1 || planet > 8)
+            {
+                Console.WriteLine("Error: Invalid planet number.\n");
+                // Re-enter planet number
+                Console.Write("Enter a valid planet number: ");
+                planet = int.Parse(Console.ReadLine());
+            }
 
 
-                while (planet < 1 || planet > 8)
-                {
-                    Console.WriteLine("Error: Invalid planet number.\n");
-                    // Re-enter planet number
-                    Console.Write("Enter a valid planet number: ");
-                    planet = int.Parse(Console.ReadLine());
-                }
+            // Your Age
+            Console.WriteLine("How old are you?");
+            int userAge = int.Parse(Console.ReadLine());
 
 
-                // Your Age
-                Console.WriteLine("How old are you?");
-                int userAge = int.Parse(Console.ReadLine());
+            // Length of years of planets (in Earth years)
+            double mercuryYears = 0.2;
+            double venusYears = 0.6;
+            double marsYears = 1.9;
+            double jupiterYears = 11.9;
+            double saturnYears = 29.4;
+            double uranusYears = 84.0;
+            double neptuneYears = 165.0;
 
 
-                // Length of years of planets (in Earth years)
-                double mercuryYears = 0.2;
-                double venusYears = 0.6;
-                double marsYears = 1.9;
-                double jupiterYears = 11.9;
-                double saturnYears = 29.4;
-                double uranusYears = 84.0;
-                double neptuneYears = 165.0;
+            // Age on planets
+            double mercuryAge = userAge / mercuryYears;
+            double venusAge = userAge / venusYears;
+            double marsAge = userAge / marsYears;
+            double jupiterAge = userAge / jupiterYears;
+            double saturnAge = userAge / saturnYears;
+            double uranusAge = userAge / uranusYears;
+            double neptuneAge = userAge / neptuneYears;
 
 
-                // Age on planets
-                double mercuryAge = userAge / mercuryYears;
-                double venusAge = userAge / venusYears;
-                double marsAge = userAge / marsYears;
-                double jupiterAge = userAge / jupiterYears;
-                double saturnAge = userAge / saturnYears;
-                double uranusAge = userAge / uranusYears;
-                double neptuneAge = userAge / neptuneYears;
+            // Time to planets
+            double journeyToMercury = 3.449692;
+            double journeyToVenus = 0.4188912;
+            double journeyToMars = 0.4435319;
+            double journeyToJupiter = 6.142466;
+            double journeyToSaturn = 6.598221;
+            double journeyToUranus = 9.598905;
+            double journeyToNeptune = 11.99726;
 
 
-                // Time to planets
-                double journeyToMercury = 3.449692;
-                double journeyToVenus = 0.4188912;
-                double journeyToMars = 0.4435319;
-                double journeyToJupiter = 6.142466;
-                double journeyToSaturn = 6.598221;
-                double journeyToUranus = 9.598905;
-                double journeyToNeptune = 11.99726;
+            // New Age on Earth
+            double newEarthAgeMercury = userAge + journeyToMercury;
+            double newEarthAgeVenus = userAge + journeyToVenus;
+            double newEarthAgeMars = userAge + journeyToMars;
+            double newEarthAgeJupiter = userAge + journeyToJupiter;
+            double newEarthAgeSaturn = userAge + journeyToSaturn;
+            double newEarthAgeUranus = userAge + journeyToUranus;
+            double newEarthAgeNeptune = userAge + journeyToNeptune;
 
 
-                // New Age on Earth
-                double newEarthAgeMercury = userAge + journeyToMercury;
-                double newEarthAgeVenus = userAge + journeyToVenus;
-                double newEarthAgeMars = userAge + journeyToMars;
-                double newEarthAgeJupiter = userAge + journeyToJupiter;
-                double newEarthAgeSaturn = userAge + journeyToSaturn;
-                double newEarthAgeUranus = userAge + journeyToUranus;
-                double newEarthAgeNeptune = userAge + journeyToNeptune;
-
-
-                // New Age on planets
-                double newMercuryAge = newEarthAgeJupiter / mercuryYears;
-                double newVenusAge = newEarthAgeJupiter / venusYears;
-                double newMarsAge = newEarthAgeJupiter / marsYears;
-                double newJupiterAge = newEarthAgeJupiter / jupiterYears;
-                double newSaturnAge = newEarthAgeJupiter / saturnYears;
-                double newUranusAge = newEarthAgeJupiter / uranusYears;
-                double newNeptuneAge = newEarthAgeJupiter / neptuneYears;
+            // New Age on planets
+            double newMercuryAge = newEarthAgeJupiter / mercuryYears;
+            double newVenusAge = newEarthAgeJupiter / venusYears;
+            double newMarsAge = newEarthAgeJupiter / marsYears;
+            double newJupiterAge = newEarthAgeJupiter / jupiterYears;
+            double newSaturnAge = newEarthAgeJupiter / saturnYears;
+            double newUranusAge = newEarthAgeJupiter / uranusYears;
+            double newNeptuneAge = newEarthAgeJupiter / neptuneYears;
 
 
             // Log calculations to console
@@ -145,7 +144,7 @@ namespace PlanetCalculations
                 Console.WriteLine("Your new age on Neptune would be: " + newNeptuneAge + " years old.");
             }
 
-                Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
