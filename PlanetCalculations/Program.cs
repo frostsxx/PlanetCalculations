af6529b5189d36110af61ef47bd566c3f85c30c0
frostsxx <henrique.geadas@gmail.com>
2023-04-10 21:13:27 +0100

    added arrays and functions

--- PlanetCalculations/Program.cs
+++ PlanetCalculations/Program.cs
@@ -37,125 +37,125 @@ namespace PlanetCalculations
 
 
             // Length of years of planets (in Earth years)
-            double mercuryYears = 0.2;
-            double venusYears = 0.6;
-            double marsYears = 1.9;
-            double jupiterYears = 11.9;
-            double saturnYears = 29.4;
-            double uranusYears = 84.0;
-            double neptuneYears = 165.0;
+            double[] planetYears = { 0.2, 0.6, 1.0, 1.9, 11.9, 29.4, 84.0, 165.0 };
+
+            /*
+            mercuryYears = 0.2;
+            venusYears = 0.6;
+            earthYears = 1.0;
+            marsYears = 1.9;
+            jupiterYears = 11.9;
+            saturnYears = 29.4;
+            uranusYears = 84.0;
+            neptuneYears = 165.0;
+            */
 
 
             // Age on planets
-            double mercuryAge = userAge / mercuryYears;
-            double venusAge = userAge / venusYears;
-            double marsAge = userAge / marsYears;
-            double jupiterAge = userAge / jupiterYears;
-            double saturnAge = userAge / saturnYears;
-            double uranusAge = userAge / uranusYears;
-            double neptuneAge = userAge / neptuneYears;
-
+            double[] planetAge = new double[8];
+            for(int index = 0; index < planetAge.Length; index++)
+            {
+                planetAge[index] = userAge / planetYears[index];
+            }
 
             // Time to planets
-            double journeyToMercury = 3.449692;
-            double journeyToVenus = 0.4188912;
-            double journeyToMars = 0.4435319;
-            double journeyToJupiter = 6.142466;
-            double journeyToSaturn = 6.598221;
-            double journeyToUranus = 9.598905;
-            double journeyToNeptune = 11.99726;
+            double[] journeyTimes = { 3.449692, 0.4188912, 0.0, 0.4435319, 6.142466, 6.598221, 9.598905, 11.99726 };
+
+            /*
+            journeyToMercury = 3.449692;
+            journeyToVenus = 0.4188912;
+            journeyToEarth = 0.0;
+            journeyToMars = 0.4435319;
+            journeyToJupiter = 6.142466;
+            journeyToSaturn = 6.598221;
+            journeyToUranus = 9.598905;
+            journeyToNeptune = 11.99726;
+            */
 
 
             // New Age on Earth
-            double newEarthAgeMercury = userAge + journeyToMercury;
-            double newEarthAgeVenus = userAge + journeyToVenus;
-            double newEarthAgeMars = userAge + journeyToMars;
-            double newEarthAgeJupiter = userAge + journeyToJupiter;
-            double newEarthAgeSaturn = userAge + journeyToSaturn;
-            double newEarthAgeUranus = userAge + journeyToUranus;
-            double newEarthAgeNeptune = userAge + journeyToNeptune;
-
+            double[] newEarthAge = new double[8];
+            for (int index = 0; index < newEarthAge.Length; index++)
+            {
+                newEarthAge[index] = userAge + journeyTimes[index];
+            }
 
             // New Age on planets
-            double newMercuryAge = newEarthAgeJupiter / mercuryYears;
-            double newVenusAge = newEarthAgeJupiter / venusYears;
-            double newMarsAge = newEarthAgeJupiter / marsYears;
-            double newJupiterAge = newEarthAgeJupiter / jupiterYears;
-            double newSaturnAge = newEarthAgeJupiter / saturnYears;
-            double newUranusAge = newEarthAgeJupiter / uranusYears;
-            double newNeptuneAge = newEarthAgeJupiter / neptuneYears;
-
+            double[] newPlanetAge = new double[8];
+            for (int index = 0; index < newPlanetAge.Length; index++)
+            {
+                newPlanetAge[index] = newEarthAge[index] / planetYears[index];
+            }
 
             // Log calculations to console
-            if (planet == 1)
+            switch (planet)
             {
-                Console.WriteLine("Your age on Earth is: " + userAge + " years old.");
-                Console.WriteLine("Your age on Mercury is: " + mercuryAge + " years old.\n");
-                Console.WriteLine("If you were to fly to Mercury... \n ");
-                Console.WriteLine("Your new age on Earth would be: " + newEarthAgeMercury + " years old.");
-                Console.WriteLine("Your new age on Mercury would be: " + newMercuryAge + " years old.");
-            }
+                case 1:
+                    Console.WriteLine($"Your age on Earth is: {userAge} years old.");
+                    Console.WriteLine($"Your age on Mercury is: {planetAge[0]} years old.\n");
+                    Console.WriteLine("If you were to fly to Mercury... \n ");
+                    Console.WriteLine($"Your new age on Earth would be: {newEarthAge[0]} years old.");
+                    Console.WriteLine($"Your new age on Mercury would be: {newPlanetAge[0]} years old.");
+                    break;
 
-            else if (planet == 2)
-            {
-                Console.WriteLine("Your age on Earth is: " + userAge + " years old.");
-                Console.WriteLine("Your age on Venus is: " + venusAge + " years old.\n");
-                Console.WriteLine("If you were to fly to Venus... \n ");
-                Console.WriteLine("Your new age on Earth would be: " + newEarthAgeVenus + " years old.");
-                Console.WriteLine("Your new age on Venus would be: " + newVenusAge + " years old.");
-            }
+                case 2:
+                    Console.WriteLine($"Your age on Earth is: {userAge} years old.");
+                    Console.WriteLine($"Your age on Venus is: {planetAge[1]} years old.\n");
+                    Console.WriteLine("If you were to fly to Venus... \n ");
+                    Console.WriteLine($"Your new age on Earth would be: {newEarthAge[1]} years old.");
+                    Console.WriteLine($"Your new age on Venus would be: {newPlanetAge[1]} years old.");
+                    break;
 
-            else if (planet == 3)
-            {
-                Console.WriteLine("Dummy you have " + userAge + " years old! :P");
-            }
+                case 3:
+                    Console.WriteLine($"Dummy you have {userAge} years old! :P");
+                    break;
 
-            else if (planet == 4)
-            {
-                Console.WriteLine("Your age on Earth is: " + userAge + " years old.");
-                Console.WriteLine("Your age on Mars is: " + marsAge + " years old.\n");
-                Console.WriteLine("If you were to fly to Mars... \n ");
-                Console.WriteLine("Your new age on Earth would be: " + newEarthAgeMars + " years old.");
-                Console.WriteLine("Your new age on Mars would be: " + newMarsAge + " years old.");
-            }
+                case 4:
 
-            else if (planet == 5)
-            {
-                Console.WriteLine("Your age on Earth is: " + userAge + " years old.");
-                Console.WriteLine("Your age on Jupiter is: " + jupiterAge + " years old.\n");
-                Console.WriteLine("If you were to fly to Jupiter... \n ");
-                Console.WriteLine("Your new age on Earth would be: " + newEarthAgeJupiter + " years old.");
-                Console.WriteLine("Your new age on Jupiter would be: " + newJupiterAge + " years old.");
-            }
+                    Console.WriteLine($"Your age on Earth is: {userAge} years old.");
+                    Console.WriteLine($"Your age on Mars is: {planetAge[3]} years old.\n");
+                    Console.WriteLine("If you were to fly to Mars... \n ");
+                    Console.WriteLine($"Your new age on Earth would be: {newEarthAge[3]} years old.");
+                    Console.WriteLine($"Your new age on Mars would be: {newPlanetAge[3]} years old.");
+                    break;
 
-            else if (planet == 6)
-            {
-                Console.WriteLine("Your age on Earth is: " + userAge + " years old.");
-                Console.WriteLine("Your age on Saturn is: " + saturnAge + " years old.\n");
-                Console.WriteLine("If you were to fly to Saturn... \n ");
-                Console.WriteLine("Your new age on Earth would be: " + newEarthAgeSaturn + " years old.");
-                Console.WriteLine("Your new age on Saturn would be: " + newSaturnAge + " years old.");
-            }
+                case 5:
+                    Console.WriteLine($"Your age on Earth is: {userAge} years old.");
+                    Console.WriteLine($"Your age on Jupiter is: {planetAge[4]} years old.\n");
+                    Console.WriteLine("If you were to fly to Jupiter... \n ");
+                    Console.WriteLine($"Your new age on Earth would be: {newEarthAge[4]} years old.");
+                    Console.WriteLine($"Your new age on Jupiter would be: {newPlanetAge[4]} years old.");
+                    break;
 
-            else if (planet == 7)
-            {
-                Console.WriteLine("Your age on Earth is: " + userAge + " years old.");
-                Console.WriteLine("Your age on Uranus is: " + uranusAge + " years old.\n");
-                Console.WriteLine("If you were to fly to Uranus... \n ");
-                Console.WriteLine("Your new age on Earth would be: " + newEarthAgeUranus + " years old.");
-                Console.WriteLine("Your new age on Uranus would be: " + newUranusAge + " years old.");
-            }
+                case 6:
+                    Console.WriteLine($"Your age on Earth is: {userAge} years old.");
+                    Console.WriteLine($"Your age on Saturn is: {planetAge[5]} years old.\n");
+                    Console.WriteLine("If you were to fly to Saturn... \n ");
+                    Console.WriteLine($"Your new age on Earth would be: {newEarthAge[5]} years old.");
+                    Console.WriteLine($"Your new age on Saturn would be: {newPlanetAge[5]} years old.");
+                    break;
 
-            else if (planet == 8)
-            {
-                Console.WriteLine("Your age on Earth is: " + userAge + " years old.");
-                Console.WriteLine("Your age on Neptune is: " + neptuneAge + " years old.\n");
-                Console.WriteLine("If you were to fly to Neptune... \n ");
-                Console.WriteLine("Your new age on Earth would be: " + newEarthAgeNeptune + " years old.");
-                Console.WriteLine("Your new age on Neptune would be: " + newNeptuneAge + " years old.");
+                case 7:
+                    Console.WriteLine($"Your age on Earth is: {userAge} years old.");
+                    Console.WriteLine($"Your age on Uranus is: {planetAge[6]} years old.\n");
+                    Console.WriteLine("If you were to fly to Uranus... \n ");
+                    Console.WriteLine($"Your new age on Earth would be: {newEarthAge[6]} years old.");
+                    Console.WriteLine($"Your new age on Uranus would be: {newPlanetAge[6]} years old.");
+                    break;
+
+                case 8:
+                    Console.WriteLine($"Your age on Earth is: {userAge} years old.");
+                    Console.WriteLine($"Your age on Neptune is: {planetAge[7]} years old.\n");
+                    Console.WriteLine("If you were to fly to Neptune... \n ");
+                    Console.WriteLine($"Your new age on Earth would be: {newEarthAge[7]} years old.");
+                    Console.WriteLine($"Your new age on Neptune would be: {newPlanetAge[7]} years old.");
+                    break;
             }
 
             Console.ReadLine();
+
         }
+
     }
 }
+
